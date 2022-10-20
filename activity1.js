00a3095a17a0f6e36f184a1887788dabb3c474b9
lvlytorno <111828209+lvlytorno@users.noreply.github.com>
2022-10-20 17:19:09 +0800

    commit

--- activity1.js
+++
@@ -1,9 +0,0 @@
-const strng = 'the quick brown fox';
-
-const arry = strng.split(" ");
-for (var i = 0; i < arry.length; i++) {
-    arry[i] = arry[i].charAt(0).toUpperCase() + arry[i].slice(1);
-}
-
-const strng2 = arry.join(" ");
-console.log(strng2);
